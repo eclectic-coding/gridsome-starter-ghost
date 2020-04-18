e7fa8ef0262c414e60ca14b56d54d079081c9f89
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-04-18 17:07:16 -0400

    Rework Author component

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -8,12 +8,12 @@
 		</h1>
 
 		<p class="author__intro">
-			A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.
+			A simple starter for Gridsome that uses Ghost for Headless content.
 		</p>
 
 		<p class="author__links">
-			<a href="//twitter.com/gridsome">Follow on Twitter</a>
-			<a href="//github.com/gridsome/gridsome-starter-blog">GitHub</a>
+			<a href="//twitter.com/ElecticCoding">Follow on Twitter</a>
+			<a href="//github.com/eclectic-coding/gridsome-starter-ghost">GitHub</a>
 		</p>
 
 	</div>
